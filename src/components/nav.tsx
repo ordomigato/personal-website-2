e16c3139674888b6dd052670b956684e15869c33
jeremygervais <jeremygervais@hotmail.com>
2023-04-12 21:27:49 -0400

    update contact form nav link

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -8,7 +8,7 @@ const Nav: React.FC = () => {
       <ul>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#contact-form">Contact</a></li>
         <li><a href={resume} target="_blank" className="resume-button">Resume</a></li>
       </ul>
     </nav>
